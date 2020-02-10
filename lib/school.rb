77909397eecd2a93b1800644d9afbbf67da6f1a8
Mark Drehmann <markdrehmann@gmail.com>
2020-02-10 03:55:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,8 +2,6 @@ class School
 
   attr_accessor :name, :roster, :grade
 
-
-
   def initialize(name)
     @name = name
     @roster = {} #key is grade, value is array of students
@@ -14,7 +12,6 @@ class School
       @roster[grade] = []
     end
     @roster[grade] << student
-
   end
 
 

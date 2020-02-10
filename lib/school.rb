8fa5be1de6a6c520fbb24bd67c3aa0ee995d5d33
Mark Drehmann <markdrehmann@gmail.com>
2020-02-10 03:31:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,17 @@
 class School
 
-  attr_accessor :name, :roster
+  attr_accessor :name, :roster, :grade
 
 
 
   def initialize(name)
     @name = name
     @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster << student
+    @grade = grade
   end
 
 
